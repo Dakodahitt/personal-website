768d6989a52f0b28fe224f1d318052564a98d6b2
Dakoda hitt <dakoda@MacBook-Air.local>
2024-02-19 18:36:10 -0700

    remvode file

---
+++ Contact-form.js
@@ -0,0 +1,12 @@
+// c// after click open new window
+//function load(){alert("sdhua")}
+function load(){
+    document.open
+    document.write("<h1>test<h1>")
+}
+
+
+// document.getElementById("myButton").onclick = function load() {
+    // alert("test") 
+    // }
+
--- website/Contact-form.js
+++
@@ -1,5 +0,0 @@
-// after click open new window
-function load(){
-    document.open()
-    document.write("<h1>Successfully sent.<h1>")
-}
