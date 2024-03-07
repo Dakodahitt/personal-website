d8649aaa0c13a50dc9022ef03b212c8854924615
Dakoda hitt <dakoda@MacBook-Air.local>
2024-03-07 09:08:42 -0700

    canged how the from sbumits and added a html for it

--- Contact-form.js
+++ Contact-form.js
@@ -1,12 +1,6 @@
-// c// after click open new window
+var html = `open.html`;
+// after click open new window
 //function load(){alert("sdhua")}
-function load(){
-    document.open
-    document.write("<h1>test<h1>")
+function load() {
+  window.open(html);
 }
-
-
-// document.getElementById("myButton").onclick = function load() {
-    // alert("test") 
-    // }
-
